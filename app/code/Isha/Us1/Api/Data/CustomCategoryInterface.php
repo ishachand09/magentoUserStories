a70a5b08c2c3b0584e18c983cb7050392f6f81fa
ishachand09 <isha.chand@hbwsl.com>
2023-11-04 14:49:54 +0530

    Created the preference for \Magento\Catalog\Api\Data\CategoryInterface in our module

---
+++ app/code/Isha/Us1/Api/Data/CustomCategoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+namespace Isha\Us1\Api\Data;
+use Magento\Catalog\Api\Data\CategoryInterface;
+
+interface CustomCategoryInterface extends CategoryInterface
+{
+    // custom category interface
+}

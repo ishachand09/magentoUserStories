221a9eb784df7fc005be196e54be6827ea5b73cf
ishachand09 <isha.chand@hbwsl.com>
2023-11-06 12:55:53 +0530

    UserStory5: Created a new module for userstory5 and also created a frontend controller that displays -Hello World- text

---
+++ app/code/Isha/Us5/Controller/Index/HelloWorld.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Isha\Us5\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class HelloWorld extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->set("Hello World");
+        return $resultPage;
+        // echo "Hello World";
+        // return $this->resultRedirectFactory->create()->setPath('catalog/product/view/id/31');
+    }
+}
---
+++ app/code/Isha/Us5/registration.php
@@ -0,0 +1,4 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Isha_Us5', __DIR__);
